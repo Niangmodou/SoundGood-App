9d098ab30de997d695067a0ff597fc4fb2e158a8
Niangmodou <niangmodou100@gmail.com>
2021-10-09 17:48:16 -0400

    feat: added database models

--- backend/app.py
+++ backend/app.py
@@ -1,6 +1,12 @@
 from flask import Flask
+from dotenv import dotenv_values
+
+config = dotenv_values(".env")
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = config['POSTGRES_URI']
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+app.secret_key = config['SECRET_KEY']
 
 @app.route("/")
 def home():
---
+++ backend/models.py
@@ -0,0 +1,57 @@
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import Table, Column, Integer, ForeignKey
+from .app import app
+
+db = SQLAlchemy(app)
+
+class User(db.Model):
+    __tablename__ = 'user'
+
+    id = db.Column(db.Integer, primary_key = True)
+
+    first_name = db.Column(db.String())
+    last_name = db.Column(db.String())
+    username = db.Column(db.String())
+    password = db.Column(db.String())
+    email_address = db.Column(db.String())
+    image_url = db.Column(db.String())
+    
+
+class AudioRecording(db.Model):
+    __tablename_ = 'audio_recording'
+
+    id = db.Column(db.Integer, primary_key = True)
+
+    user_id = Column(Integer, ForeignKey('user.id'))
+    sound_url = db.Column(db.String())
+
+
+class Post(db.Model):
+    __tablename__ = 'post'
+
+    id = db.Column(db.Integer, primary_key = True)
+
+    user_id = Column(Integer, ForeignKey('user.id'))
+    audio_id = Column(Integer, ForeignKey('audio_recording.id'))
+    text = db.Column(db.String())
+    date_posted = db.Column(db.DateTime)
+
+
+class Comment(db.Model):
+    __tablename__ = 'comment'
+
+    id = db.Column(db.Integer, primary_key = True)
+
+    post_id = db.Column(db.Integer, ForeignKey('post.id'))
+
+    like_count = db.Column(db.Integer)
+    dislike_count = db.Column(db.Integer)
+
+    date_posted = db.Column(db.DateTime)
+
+
+
+
+
+
+
