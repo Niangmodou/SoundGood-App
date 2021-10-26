67f4b3b1a8ac31b3d86227237b1a770438abbebe
Niangmodou <niangmodou100@gmail.com>
2021-10-26 12:27:08 -0400

    login & register routes complete

--- backend/config.py
+++ backend/config.py
@@ -9,3 +9,6 @@ config = dotenv_values(".env")
 app.config["SQLALCHEMY_DATABASE_URI"] = config["POSTGRES_URI"]
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 app.secret_key = config["SECRET_KEY"]
+
+# Password secret SALT
+SALT = config["SALT"]
