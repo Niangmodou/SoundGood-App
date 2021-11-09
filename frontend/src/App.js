eb82ba63c276bd3c1d2c0a48d567fd3231d9e763
addyhebou <addyhebou@gmail.com>
2021-11-08 23:33:17 -0500

    feat(app): first push

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,34 @@
 import logo from './logo.svg';
+import React, { useState, Component } from 'react';
 import './App.css';
+import RedBackgroundOverlay from './Icons/RedBackgroundOverlay.png';
+// import { BsFillPersonFill as PersonIcon } from 'react-icons/BsFillPersonFill';
+// clientID: 626147039911-9ume6m1jbb1k022bmmppqk7gapkhk6aa.apps.googleusercontent.com
+// client Secret: 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
+import Background from './Images/Background.png';
+import LandingPage from './Pages/LandingPage';
 
-function App() {
+export default function App() {
+  const [isSignedIn, setIsSignedIn] = useState(true);
+
+  // onSignIn(googleUser){}
+  //   var profile = googleUser.getBasicProfile();
+  //   console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
+  //   console.log('Name: ' + profile.getName());
+  //   console.log('Image URL: ' + profile.getImageUrl());
+  //   console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
+  // }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App' style={{ backgroundImage: `url(${Background})` }}>
+      {isSignedIn ? (
+        <LandingPage />
+      ) : (
+        <header className='App-header'>
+          <h2>SoundGood</h2>
+          <div class='g-signin2' data-onsuccess='onSignIn'></div>
+        </header>
+      )}
     </div>
   );
 }
-
-export default App;
