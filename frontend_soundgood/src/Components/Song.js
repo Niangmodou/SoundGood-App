4581b1d27e9f68622b6e7896290181954cfa8027
addyhebou <addyhebou@gmail.com>
2021-11-10 23:25:32 -0500

    feat(profile): completed UI for profile, separated out CSS files, working on DiscoveredSongs page

---
+++ frontend_soundgood/src/Components/Song.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { FaPlayCircle } from 'react-icons/fa';
+import '../Css/SongComponent.css';
+
+export default function Component() {
+  return (
+    <div className='songComponent'>
+      <div className='songText'>
+        <strong>You're Beautiful</strong>
+        <p>James Blunt</p>
+      </div>
+      <div>
+        <FaPlayCircle />
+      </div>
+      <div></div>
+    </div>
+  );
+}
